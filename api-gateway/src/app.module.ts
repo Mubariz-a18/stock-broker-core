eb426f6fe516a9c434924be8d7fc9006da72684d
mubariz542 <syedmubariz542@gmail.com>
2026-01-12 17:06:57 +0530

    initial-commit

---
+++ api-gateway/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { MarketModule } from './modules/market/market.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { PortfolioModule } from './modules/portfolio/portfolio.module';
+import { WatchlistModule } from './modules/watchlist/watchlist.module';
+import { AlertsModule } from './modules/alerts/alerts.module';
+
+@Module({
+  imports: [UsersModule, AuthModule, MarketModule, OrdersModule, PortfolioModule, WatchlistModule, AlertsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
